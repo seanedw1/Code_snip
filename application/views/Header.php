e24d8f481169a23a743053ecfe2fb88712f8fd79
seanedw1 <sedwards@fullsail.edu>
2017-04-26 23:25:55 -0400

    created a header file

---
+++ application/views/Header.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Home</title>
+  <link href="assets/css/bootstrap.css" rel="stylesheet">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
+</head>
+<body>
